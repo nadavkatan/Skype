7417f599820d12fcbcb6f7453bbbcff4833a26e4
nadavkatan <nadavkatan110@gmail.com>
2022-06-21 17:57:43 +0200

    config dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ silent: process.env.NODE_ENV === 'production' });
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
