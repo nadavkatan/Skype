572b7a5b82b777e9610f3eb53e8eaf996f245253
nadavkatan <nadavkatan110@gmail.com>
2022-06-25 20:05:34 +0200

    fix bug

--- frontend/src/app/pages/editPage/EditPage.jsx
+++ frontend/src/app/pages/editPage/EditPage.jsx
@@ -43,96 +43,134 @@ const EditPage = () => {
   };
 
   return (
-    <Box className={classes.editPageWrapper}>
-      <Box className={isSmallScreen? classes.smScreenEditPageContainer :classes.editPageContainer}>
-        <Box className={classes.editPageHeader}>
-          <Typography variant="h3">Edit profile</Typography>
-          <Typography variant="subtitle1">
-            People on Skype will get to know you with the info below
-          </Typography>
-        </Box>
-        <Grid container spacing={2}>
-          <Grid item md={6} xs={12}></Grid>
-          <Grid item md={6} xs={12}></Grid>
-        </Grid>
-        <Box
-          className={classes.editPageFormContainer}
-          component="form"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <Box className={isSmallScreen? classes.smScreenFormHeader :classes.formHeader}>
-            <Box className={isSmallScreen? classes.smScreenEditPageAvatarContainer : classes.editPageAvatarContainer}>
-              <div className={classes.editPageAvatar}>
-                <Avatar
-                  avatarDimensions={{ width: 150, height: 150 }}
-                  imgSrc={currentUser.avatar.secure_url}
-                  loggedIn={currentUser.is_logged_in}
-                />
-              </div>
-              <Button variant="outlined" component="label" className={isSmallScreen? classes.smScreenChangePictureBtn : null}>
-                Change profile picture
-                <input
-                  style={{
-                    width: "100%",
-                    position: "absolute",
-                    display: "none",
-                  }}
-                  type="file"
-                  accept="image/png, image/jpeg"
-                  {...register("avatar")}
-                />
-              </Button>
+    <>
+      {currentUser && (
+        <Box className={classes.editPageWrapper}>
+          <Box
+            className={
+              isSmallScreen
+                ? classes.smScreenEditPageContainer
+                : classes.editPageContainer
+            }
+          >
+            <Box className={classes.editPageHeader}>
+              <Typography variant="h3">Edit profile</Typography>
+              <Typography variant="subtitle1">
+                People on Skype will get to know you with the info below
+              </Typography>
             </Box>
-            <Button type="submit" variant="contained">
-              Done
+            <Grid container spacing={2}>
+              <Grid item md={6} xs={12}></Grid>
+              <Grid item md={6} xs={12}></Grid>
+            </Grid>
+            <Box
+              className={classes.editPageFormContainer}
+              component="form"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <Box
+                className={
+                  isSmallScreen
+                    ? classes.smScreenFormHeader
+                    : classes.formHeader
+                }
+              >
+                <Box
+                  className={
+                    isSmallScreen
+                      ? classes.smScreenEditPageAvatarContainer
+                      : classes.editPageAvatarContainer
+                  }
+                >
+                  <div className={classes.editPageAvatar}>
+                    <Avatar
+                      avatarDimensions={{ width: 150, height: 150 }}
+                      imgSrc={currentUser.avatar.secure_url}
+                      loggedIn={currentUser.is_logged_in}
+                    />
+                  </div>
+                  <Button
+                    variant="outlined"
+                    component="label"
+                    className={
+                      isSmallScreen ? classes.smScreenChangePictureBtn : null
+                    }
+                  >
+                    Change profile picture
+                    <input
+                      style={{
+                        width: "100%",
+                        position: "absolute",
+                        display: "none",
+                      }}
+                      type="file"
+                      accept="image/png, image/jpeg"
+                      {...register("avatar")}
+                    />
+                  </Button>
+                </Box>
+                <Button type="submit" variant="contained">
+                  Done
+                </Button>
+              </Box>
+              <Grid container spacing={2}>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    defaultValue={currentUser.first_name}
+                    {...register("first_name")}
+                  />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    defaultValue={currentUser.last_name}
+                    {...register("last_name")}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    defaultValue={currentUser.username}
+                    {...register("username")}
+                  />
+                  {errors.username && <span>{errors.username.message}</span>}
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    defaultValue={currentUser.email}
+                    {...register("email", {
+                      pattern: {
+                        value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                        message: "Please enter a valid email address",
+                      },
+                    })}
+                  />
+                  {errors.email && <span>{errors.email.message}</span>}
+                </Grid>
+              </Grid>
+            </Box>
+            <ChangePasswordForm />
+          </Box>
+          <Box
+            className={
+              isSmallScreen
+                ? classes.smScreenBackBtnContainer
+                : classes.backBtnContainer
+            }
+          >
+            <Button
+              variant="contained"
+              onClick={() => navigate("/")}
+              className={isSmallScreen ? classes.smScreenBackBtn : null}
+            >
+              Back
             </Button>
           </Box>
-          <Grid container spacing={2}>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                defaultValue={currentUser.first_name}
-                {...register("first_name")}
-              />
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                defaultValue={currentUser.last_name}
-                {...register("last_name")}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                defaultValue={currentUser.username}
-                {...register("username")}
-              />
-              {errors.username && <span>{errors.username.message}</span>}
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                defaultValue={currentUser.email}
-                {...register("email", {
-                  pattern: {
-                    value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-                    message: "Please enter a valid email address",
-                  },
-                })}
-              />
-              {errors.email && <span>{errors.email.message}</span>}
-            </Grid>
-          </Grid>
         </Box>
-        <ChangePasswordForm />
-      </Box>
-      <Box className={isSmallScreen? classes.smScreenBackBtnContainer : classes.backBtnContainer}>
-        <Button variant="contained" onClick={() => navigate("/")} className={isSmallScreen? classes.smScreenBackBtn: null}>
-          Back
-        </Button>
-      </Box>
-    </Box>
+      )}
+    </>
   );
 };
 
